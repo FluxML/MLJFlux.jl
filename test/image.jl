e9e8249a57fdb7b86130306f59bf1dbb2fd39d15
ayush-1506 <ayush.shridhar1506@gmail.com>
2020-04-09 15:27:14 +1000

    mnist test

--- test/image.jl
+++ test/image.jl
@@ -1,8 +1,16 @@
+mutable struct mynn <: MLJFlux.Builder
+    kernel1
+    kernel2
+end
+
+mutable struct mnistclassifier <: MLJFlux.Builder
+    kernel1
+    filters1
+    kernel2
+    filters2
+end
+
 @testset "ImageClassifier" begin
-    mutable struct mynn <: MLJFlux.Builder
-        kernel1
-        kernel2
-    end
 
     function MLJFlux.fit(model::mynn, ip, op)
         return Flux.Chain(Flux.Conv(model.kernel1, 1=>2), Flux.Conv(model.kernel2, 2=>1), vec, Flux.Dense(16, op))
@@ -17,6 +25,31 @@
     
     model.epochs = 15
     MLJBase.update(model, 3, fitresult, cache, img, l)
+end
+
+@testset "Image MNIST" begin
+    using Flux.Data:MNIST
+
+    images, labels = MNIST.images(), MNIST.labels()
+
+    # Images here are of dimension 28x28
+    # They need to be 28x28x1 according to the 
+    # convention.
+    images = [reshape(image, 28, 28, 1) for image in images]
+    labels = categorical(labels)
+
+    function MLJFlux.fit(model::mnistclassifier, ip, op)
+        dense_layers = ip[1:2] .- model.kernel1 .+ 1 .- model.kernel2 .+ 1 |> prod
+
+        return Flux.Chain(Flux.Conv(model.kernel1, 1=>model.filters1), 
+                            Flux.Conv(model.kernel2, model.filters1=>model.filters2), vec,
+                                Flux.Dense(dense_layers, op))
+    end
+
+    model = MLJFlux.ImageClassifier(builder=mnistclassifier((3,3), 2, (3,3), 1))
+
+    fitresult, cache, report = MLJBase.fit(model, 3, images[1:500], labels[1:500])
+
 end
 
 true

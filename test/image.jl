077113e823f83b54b0ea06d356dc2d4740017659
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-05-27 18:45:12 +1200

    update first image test set to reflect API change

--- test/image.jl
+++ test/image.jl
@@ -12,19 +12,29 @@ end
 
 @testset "ImageClassifier" begin
 
-    function MLJFlux.fit(model::mynn, ip, op)
-        return Flux.Chain(Flux.Conv(model.kernel1, 1=>2), Flux.Conv(model.kernel2, 2=>1), vec, Flux.Dense(16, op))
-    end
+    MLJFlux.fit(model::mynn, ip, op) =
+        Flux.Chain(Flux.Conv(model.kernel1, 1=>2),
+                   Flux.Conv(model.kernel2, 2=>1),
+                   vec,
+                   Flux.Dense(16, op))
 
     model = MLJFlux.ImageClassifier(builder = mynn((2,2), (2,2)), epochs=10)
-    img, l = [rand(6,6,1) for i=1:50], CategoricalArray(rand(1:5, 50))
 
-    fitresult, cache, report = MLJBase.fit(model, 3, img, l)
+    # collection of gray images as a 4D array in WHCN format:
+    raw_images = rand(6, 6, 1, 50);
 
-    pred = MLJBase.predict(model, fitresult, img[1:5])
-    
+    # as a vector of Matrix{<:AbstractRGB}
+    images = coerce(raw_images, GrayImage);
+    @test scitype(images) == AbstractVector{GrayImage{6,6}}
+
+    labels = categorical(rand(1:5, 50));
+
+    fitresult, cache, report = MLJBase.fit(model, 3, images, labels)
+
+    pred = MLJBase.predict(model, fitresult, images[1:5])
+
     model.epochs = 15
-    MLJBase.update(model, 3, fitresult, cache, img, l)
+    MLJBase.update(model, 3, fitresult, cache, images, labels)
 end
 
 @testset "Image MNIST" begin
@@ -33,7 +43,7 @@ end
     images, labels = MNIST.images(), MNIST.labels()
 
     # Images here are of dimension 28x28
-    # They need to be 28x28x1 according to the 
+    # They need to be 28x28x1 according to the
     # convention.
     images = [reshape(image, 28, 28, 1) for image in images]
     labels = categorical(labels)
@@ -41,7 +51,7 @@ end
     function MLJFlux.fit(model::mnistclassifier, ip, op)
         dense_layers = ip[1:2] .- model.kernel1 .+ 1 .- model.kernel2 .+ 1 |> prod
 
-        return Flux.Chain(Flux.Conv(model.kernel1, 1=>model.filters1), 
+        return Flux.Chain(Flux.Conv(model.kernel1, 1=>model.filters1),
                             Flux.Conv(model.kernel2, model.filters1=>model.filters2), vec,
                                 Flux.Dense(dense_layers, op))
     end

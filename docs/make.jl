56e65fb58daf4833b184692e150c9188d8e6279f
Essam <essamwisam@outlook.com>
2024-05-22 14:36:23 +0300

    🎨 Minor style improvements

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,16 @@ DocMeta.setdocmeta!(MLJFlux, :DocTestSetup, :(using MLJFlux); recursive=true)
 
 makedocs(
 	sitename = "MLJFlux",
-	format = Documenter.HTML(repolink="https://github.com/FluxML/MLJFlux.jl"),
+    format = Documenter.HTML(;
+    assets = [
+        "assets/favicon.ico",
+        asset(
+            "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+            class = :css,
+        ),
+    ],
+    repolink="https://github.com/FluxML/MLJFlux.jl"
+),
 	modules = [MLJFlux],
 	warnonly = true,
 	pages = ["Introduction" => "index.md",

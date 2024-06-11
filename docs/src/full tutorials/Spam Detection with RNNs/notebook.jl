5cd9d9bd46c0a5899f77ea206c48ad5becdaf734
Anthony D. Blaom <anthony.blaom@gmail.com>
2024-06-11 14:52:26 +1200

    fix Spam tutorial so it executes as self-contained tutorial

--- docs/src/full tutorials/Spam Detection with RNNs/notebook.jl
+++ docs/src/full tutorials/Spam Detection with RNNs/notebook.jl
@@ -1,15 +1,18 @@
 # # SMS Spam Detection with RNNs
 
-# In this tutorial we use a custom RNN model from Flux with MLJFlux to classify text messages as spam or ham. We will be using the [SMS Collection Dataset](https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset) from Kaggle.
+# In this tutorial we use a custom RNN model from Flux with MLJFlux to classify text
+# messages as spam or ham. We will be using the [SMS Collection
+# Dataset](https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset) from Kaggle.
 
-using Pkg     #src
-Pkg.activate(@__DIR__);     #src
-Pkg.instantiate();     #src
+using Pkg #!md
+Pkg.activate(@__DIR__); #!md
+Pkg.instantiate(); #!md
 
 # ### Basic Imports
 using MLJ
 using MLJFlux
 using Flux
+import Optimisers       # Flux.jl native optimisers no longer supported
 using CSV               # Read data
 using DataFrames        # Read data
 using ScientificTypes   # Type coercion
@@ -18,54 +21,69 @@ using Languages         # For stop words
 
 
 # ### Reading Data
-df = CSV.read("./sms.csv", DataFrame);
+
+# We assume the [SMS Collection
+# Dataset](https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset) has been
+# downloaded and is in a file called "sms.csv" in the same directory as the this script.
+df = CSV.read(joinpath(@__DIR__, "sms.csv"), DataFrame);
 
 # Display the first 5 rows with DataFrames
-first(df, 5) |> pretty
+first(df, 5)
 
 
 # ### Text Preprocessing
 # Let's define a function that given an SMS message would:
 # - Tokenize it (i.e., convert it into a vector of words)
 
-# - Remove stop words (i.e., words that are not useful for the analysis, like "the", "a", etc.)
+# - Remove stop words (i.e., words that are not useful for the analysis, like "the", "a",
+#   etc.)
 
 # - Return the filtered vector of words
 
+const STOP_WORDS = ["a", "an", "and", "are", "as", "at", "be", "but", "by", "for",
+                    "if", "in", "into", "is", "it", "no", "not", "of", "on", "or",
+                    "such", "that", "the", "their", "then", "there", "these", "they",
+                    "this", "to", "was", "will", "with"]
+
 function preprocess_text(text)
-	## (1) Splitting texts into words (so later it can be a sequence of vectors)
-	tokens = WordTokenizers.tokenize(text)
+    ## (1) Splitting texts into words (so later it can be a sequence of vectors)
+    tokens = WordTokenizers.tokenize(text)
 
-	## (2) Stop word removal
-	stop_words = Languages.stopwords(Languages.English())
-	filtered_tokens = filter(token -> !(token in stop_words), tokens)
+    ## (2) Stop word removal
+    top_words = Languages.stopwords(Languages.English())
+    filtered_tokens = filter(token -> !(token in STOP_WORDS), tokens)
 
-	return filtered_tokens
+    return filtered_tokens
 end
 
-# Define the vocabulary to be the set of all words in our training set. We also need a function that would map each word in a given sequence of words into its index in the dictionary (which is equivalent to representing the words as one-hot vectors).
+# Define the vocabulary to be the set of all words in our training set. We also need a
+# function that would map each word in a given sequence of words into its index in the
+# dictionary (which is equivalent to representing the words as one-hot vectors).
 
-# Now after we do this the sequences will all be numerical vectors but they will be of unequal length. Thus, to facilitate batching of data for the deep learning model, we need to decide on a specific maximum length for all sequences and:
+# Now after we do this the sequences will all be numerical vectors but they will be of
+# unequal length. Thus, to facilitate batching of data for the deep learning model, we
+# need to decide on a specific maximum length for all sequences and:
 
 # - If a sequence is longer than the maximum length, we need to truncate it
 
-# - If a sequence is shorter than the maximum length, we need to pad it with a new token 
+# - If a sequence is shorter than the maximum length, we need to pad it with a new token
 
-# Lastly, we must also handle the case that an incoming text sequence may involve words never seen in training by represent all such out-of-vocabulary words with a new token.
+# Lastly, we must also handle the case that an incoming text sequence may involve words
+# never seen in training by represent all such out-of-vocabulary words with a new token.
 
 # We will define a function that would do this for us.
 
 function encode_and_equalize(text_seq, vocab_dict, max_length, pad_val, oov_val)
-	## (1) encode using the vocabulary
-	text_seq_inds = [get(vocab_dict, word, oov_val) for word in text_seq]
+    ## (1) encode using the vocabulary
+    text_seq_inds = [get(vocab_dict, word, oov_val) for word in text_seq]
 
-	## (2) truncate sequence if > max_length
-	length(text_seq_inds) > max_length && (text_seq_inds = text_seq_inds[1:max_length])
+    ## (2) truncate sequence if > max_length
+    length(text_seq_inds) > max_length && (text_seq_inds = text_seq_inds[1:max_length])
 
-	## (3) pad with pad_val
-	text_seq_inds = vcat(text_seq_inds, fill(pad_val, max_length - length(text_seq_inds)))
+    ## (3) pad with pad_val
+    text_seq_inds = vcat(text_seq_inds, fill(pad_val, max_length - length(text_seq_inds)))
 
-	return text_seq_inds
+    return text_seq_inds
 end
 
 # ### Preparing Data
@@ -73,10 +91,13 @@ end
 x_data, y_data = unpack(df, ==(:Message), ==(:Category))
 y_data = coerce(y_data, Multiclass);
 
-(x_train, x_val), (y_train, y_val) = partition((x_data, y_data), 0.8,
-	multi = true,
-	shuffle = true,
-	rng = 42);
+(x_train, x_val), (y_train, y_val) = partition(
+    (x_data, y_data),
+    0.8,
+    multi = true,
+    shuffle = true,
+    rng = 42,
+);
 
 # Now let's process the training and validation sets:
 x_train_processed = [preprocess_text(text) for text in x_train]
@@ -94,14 +115,14 @@ max_length = 12                 # can choose this more smartly if you wish
 
 # Encode and equalize training and validation data:
 x_train_processed_equalized = [
-	encode_and_equalize(seq, vocab_dict, max_length, pad_val, oov_val) for
-	seq in x_train_processed
-]
+    encode_and_equalize(seq, vocab_dict, max_length, pad_val, oov_val) for
+        seq in x_train_processed
+        ]
 x_val_processed_equalized = [
-	encode_and_equalize(seq, vocab_dict, max_length, pad_val, oov_val) for
-	seq in x_val_processed
-]
-x_train_processed_equalized[1:5]          # all sequences are encoded and of the same length
+    encode_and_equalize(seq, vocab_dict, max_length, pad_val, oov_val) for
+        seq in x_val_processed
+        ]
+x_train_processed_equalized[1:5]        # all sequences are encoded and of the same length
 
 
 # Convert both structures into matrix form:
@@ -112,7 +133,8 @@ size(x_train_processed_equalized_fixed)
 
 # ### Instantiate Model
 
-# For the model, we will use a RNN from Flux. We will average the hidden states corresponding to any sequence then pass that to a dense layer for classification.
+# For the model, we will use a RNN from Flux. We will average the hidden states
+# corresponding to any sequence then pass that to a dense layer for classification.
 
 # For this, we need to define a custom Flux layer to perform the averaging operation:
 
@@ -120,34 +142,38 @@ struct Mean end
 Flux.@layer Mean
 (m::Mean)(x) = mean(x, dims = 2)[:, 1, :]   # [batch_size, seq_len, hidden_dim] => [batch_size, 1, hidden_dim]=> [batch_size, hidden_dim]
 
-# For compatibility, we will also define a layer that simply casts the input to integers as the embedding layer in Flux expects integers but the MLJFlux model expects floats:
+# For compatibility, we will also define a layer that simply casts the input to integers
+# as the embedding layer in Flux expects integers but the MLJFlux model expects floats:
 struct Intify end
 Flux.@layer Intify
 (m::Intify)(x) = Int.(x)
 
 # Here we define our network:
 builder = MLJFlux.@builder begin
-	Chain(
-		Intify(),                         # Cast input to integer
-		Embedding(vocab_size + 2 => 300),  # Embedding layer
-		RNN(300, 50, tanh),               # RNN layer
-		Mean(),                           # Mean pooling layer
-		Dense(50, 2)                     # Classification dense layer
-	)
+    Chain(
+        Intify(),                         # Cast input to integer
+        Embedding(vocab_size + 2 => 300), # Embedding layer
+        RNN(300, 50, tanh),               # RNN layer
+        Mean(),                           # Mean pooling layer
+        Dense(50, 2),                     # Classification dense layer
+    )
 end
 
-# Notice that we used an embedding layer with input dimensionality `vocab_size + 2` to take into account the padding and out-of-vocabulary tokens. Recall that the indices in our input correspond to one-hot-vectors and the embedding layer's purpose is to learn to map them into meaningful dense vectors (of dimensionality 300 here).
+# Notice that we used an embedding layer with input dimensionality `vocab_size + 2` to
+# take into account the padding and out-of-vocabulary tokens. Recall that the indices in
+# our input correspond to one-hot-vectors and the embedding layer's purpose is to learn to
+# map them into meaningful dense vectors (of dimensionality 300 here).
 
 # 1. Load and instantiate model
 NeuralNetworkClassifier = @load NeuralNetworkClassifier pkg = MLJFlux
 clf = NeuralNetworkClassifier(
-	builder = builder,
-	optimiser = Flux.ADAM(0.1),
-	batch_size = 128,
-	epochs = 10,
+    builder = builder,
+    optimiser = Optimisers.Adam(0.1),
+    batch_size = 128,
+    epochs = 10,
 )
 
-# 2. Wrap it in a machine 
+# 2. Wrap it in a machine
 x_train_processed_equalized_fixed = coerce(x_train_processed_equalized_fixed, Continuous)
 mach = machine(clf, x_train_processed_equalized_fixed, y_train)
 
@@ -167,14 +193,9 @@ Random.seed!(99);
 z = rand(x_val)
 z_processed = preprocess_text(z)
 z_encoded_equalized =
-	encode_and_equalize(z_processed, vocab_dict, max_length, pad_val, oov_val)
+    encode_and_equalize(z_processed, vocab_dict, max_length, pad_val, oov_val)
 z_encoded_equalized_fixed = matrixify([z_encoded_equalized])
 z_encoded_equalized_fixed = coerce(z_encoded_equalized_fixed, Continuous)
 z_pred = predict_mode(mach, z_encoded_equalized_fixed)
 
 print("SMS: `$(z)` and the prediction is `$(z_pred)`")
-
-
-using Literate #src
-Literate.markdown(@__FILE__, @__DIR__, execute = true) #src
-Literate.notebook(@__FILE__, @__DIR__, execute = true) #src

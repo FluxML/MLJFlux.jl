e01dad95b09f0a827a23a6829241597eff288818
Anthony D. Blaom <anthony.blaom@gmail.com>
2024-06-11 16:04:19 +1200

    update early stopping workflow

--- docs/src/workflow examples/Early Stopping/notebook.jl
+++ docs/src/workflow examples/Early Stopping/notebook.jl
@@ -2,9 +2,9 @@
 
 # In this workflow example, we learn how MLJFlux enables us to easily use early stopping when training MLJFlux models.
 
-using Pkg     #src
-Pkg.activate(@__DIR__);     #src
-Pkg.instantiate();     #src
+using Pkg     #!md
+Pkg.activate(@__DIR__);     #!md
+Pkg.instantiate();     #!md
 
 # **Julia version** is assumed to be 1.10.*
 
@@ -13,7 +13,8 @@ Pkg.instantiate();     #src
 using MLJ               # Has MLJFlux models
 using Flux              # For more flexibility
 import RDatasets        # Dataset source
-using Plots         # To visualize training
+using Plots             # To visualize training
+import Optimisers       # native Flux.jl optimisers no longer supported
 
 # ### Loading and Splitting the Data
 
@@ -22,22 +23,22 @@ y, X = unpack(iris, ==(:Species), colname -> true, rng=123);
 X = Float32.(X);      # To be compatible with type of network network parameters
 
 
-# ### Instantiating the model
-# Now let's construct our model. This follows a similar setup to the one followed in the [Quick Start](../../index.md#Quick-Start).
+# ### Instantiating the model Now let's construct our model. This follows a similar setup
+# to the one followed in the [Quick Start](../../index.md#Quick-Start).
 
 NeuralNetworkClassifier = @load NeuralNetworkClassifier pkg=MLJFlux
 
 clf = NeuralNetworkClassifier(
     builder=MLJFlux.MLP(; hidden=(5,4), σ=Flux.relu),
-    optimiser=Flux.ADAM(0.01),
+    optimiser=Optimisers.Adam(0.01),
     batch_size=8,
-    epochs=50, 
-    rng=42
-    )
+    epochs=50,
+    rng=42,
+)
 
 # ### Wrapping it in an IteratedModel
 
-# Let's start by defining the condition that can cause the model to early stop. 
+# Let's start by defining the condition that can cause the model to early stop.
 stop_conditions = [
     Step(1),            # Repeatedly train for one iteration
     NumberLimit(100),   # Don't train for more than 100 iterations
@@ -47,7 +48,7 @@ stop_conditions = [
 ]
 
 # We can also define callbacks. Here we want to store the validation loss for each iteration
-validation_losses =  []
+validation_losses = []
 callbacks = [
     WithLossDo(loss->push!(validation_losses, loss)),
 ]
@@ -55,17 +56,20 @@ callbacks = [
 # Construct the iterated model and pass to it the stop_conditions and the callbacks:
 iterated_model = IteratedModel(
     model=clf,
-    resampling=CV(nfolds=6),    # Split the data internally into 0.7 training and 0.3 validation
+    resampling=Holdout(fraction_train=0.7); # loss and stopping are based on out-of-sample
     measures=log_loss,
     iteration_parameter=:(epochs),
     controls=vcat(stop_conditions, callbacks),
-    retrain=false                  # no need to retrain on all data at the end
+    retrain=false            # no need to retrain on all data at the end
 );
 
-# You can see more advanced stopping conditions as well as how to involve callbacks in the [documentation](https://juliaai.github.io/MLJ.jl/stable/controlling_iterative_models/#Controlling-Iterative-Models)
+# You can see more advanced stopping conditions as well as how to involve callbacks in the
+# [documentation](https://juliaai.github.io/MLJ.jl/stable/controlling_iterative_models/#Controlling-Iterative-Models)
 
 # ### Training with Early Stopping
-# At this point, all we need is to fit the model and iteration controls will be automatically handled
+
+# At this point, all we need is to fit the model and iteration controls will be
+# automatically handled
 
 mach = machine(iterated_model, X, y)
 fit!(mach)
@@ -73,13 +77,8 @@ fit!(mach)
 training_losses = report(mach)[:model_report].training_losses;
 
 # ### Results
+
 # We can see that the model converged after 100 iterations.
 
 plot(training_losses, label="Training Loss", linewidth=2)
 plot!(validation_losses, label="Validation Loss", linewidth=2, size=(800,400))
-
-#-
-
-using Literate #src
-Literate.markdown(@__FILE__, @__DIR__, execute=false) #src
-Literate.notebook(@__FILE__, @__DIR__, execute=true) #src

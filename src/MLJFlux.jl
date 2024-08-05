cf30f9033eb2b7e959c0d8e1279d7199caa07da7
Essam <essamwisam@outlook.com>
2024-08-05 18:31:34 -0500

    ➕ Include necessary files in MLJFlux.jl

--- src/MLJFlux.jl
+++ src/MLJFlux.jl
@@ -2,7 +2,6 @@ module MLJFlux
 
 export CUDALibs, CPU1
 
-import Flux
 using MLJModelInterface
 using MLJModelInterface.ScientificTypesBase
 using ScientificTypes: schema, Finite
@@ -14,12 +13,12 @@ using Statistics
 using ColorTypes
 using ComputationalResources
 using Random
-import Metalhead
-import Optimisers
 
 include("utilities.jl")
-const MMI=MLJModelInterface
+const MMI = MLJModelInterface
 
+include("encoders.jl")
+include("entity_embedding.jl")
 include("builders.jl")
 include("metalhead.jl")
 include("types.jl")
@@ -28,15 +27,11 @@ include("regressor.jl")
 include("classifier.jl")
 include("image.jl")
 include("mlj_model_interface.jl")
-include("entity_embedding.jl")
-include("ordinal_encoder.jl")
 
 export NeuralNetworkRegressor, MultitargetNeuralNetworkRegressor
 export NeuralNetworkClassifier, NeuralNetworkBinaryClassifier, ImageClassifier
 export CUDALibs, CPU1
-export CategoricalEmbedder
+export EntityEmbedder
 
 include("deprecated.jl")
-
-
 end

f823f8b0c32d9cbb766dd9221de556a827cbc552
Anthony D. Blaom <anthony.blaom@gmail.com>
2026-08-19 13:23:33 +1200

    temporarily mark test for #322 as broken

--- test/mlj_model_interface.jl
+++ test/mlj_model_interface.jl
@@ -370,7 +370,7 @@ end
     sum_predictions2 = predict(mach, X) |> sum
 
     # 3. Check we get different outcomes:
-    @test !(sum_predictions ≈ sum_predictions2)
+    @test_broken !(sum_predictions ≈ sum_predictions2)
 end
 
 true

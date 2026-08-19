ee4b224e7333e5fb43384b2f9eafb28560c672d6
Anthony D. Blaom <anthony.blaom@gmail.com>
2026-08-19 13:02:17 +1200

    add old signatures of train and train_epoch as deprecated

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,19 +1,41 @@
-Base.@deprecate fit!(model::MLJFlux.MLJFluxModel, penalty, chain, optimiser, epochs, verbosity, X, y) train(
-    model::MLJFlux.MLJFluxModel,
-    chain,
-    optimiser,
-    optimiser_state,
-    epochs,
-    verbosity,
-    X,
-    y,
-) false
+Base.@deprecate(
+    train(
+        model::MLJFlux.MLJFluxModel,
+        chain,
+        optimiser,
+        optimiser_state,
+        epochs,
+        verbosity,
+        X,
+        y,
+    ),
+    train(
+        model::MLJFlux.MLJFluxModel,
+        chain,
+        optimiser_state,
+        epochs,
+        verbosity,
+        X,
+        y,
+    ),
+    false,
+)
 
-Base.@deprecate train!(model::MLJFlux.MLJFluxModel, penalty, chain, optimiser, X, y) train_epoch(
-    model::MLJFlux.MLJFluxModel,
-    chain,
-    optimiser,
-    optimiser_state,
-    X,
-    y,
-) false
+Base.@deprecate(
+    train_epoch(
+        model::MLJFlux.MLJFluxModel,
+        chain,
+        optimiser,
+        optimiser_state,
+        X,
+        y,
+    ),
+    train_epoch(
+        model::MLJFlux.MLJFluxModel,
+        chain,
+        optimiser_state,
+        X,
+        y,
+    ),
+    false,
+)
